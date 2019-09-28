21c2b12b3dc8eb375e7d1e7bf9ea456ac31c658b
dsonyy <dsonyyy@gmail.com>
2019-09-28 20:48:54 +0200

    updates

--- fizz-buzz/target/debug/deps/fizz_buzz-687c859c64456769.d
+++
@@ -1,5 +0,0 @@
-D:\current\rust-learning\fizz-buzz\target\debug\deps\fizz_buzz-687c859c64456769.exe: src\main.rs
-
-D:\current\rust-learning\fizz-buzz\target\debug\deps\fizz_buzz-687c859c64456769.d: src\main.rs
-
-src\main.rs:
--- fizz-buzz/target/debug/fizz-buzz.d
+++
@@ -1,1 +0,0 @@
-D:\current\rust-learning\fizz-buzz\target\debug\fizz-buzz.pdb: D:\current\rust-learning\fizz-buzz\src\main.rs
